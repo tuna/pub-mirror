d22006904e6ec60bda4e069d7ca7678da94283e7
Hui Yiqun <i@huiyiqun.me>
2019-02-19 13:42:54 +0800

    download achieve_files with dart:io

--- lib/pub_mirror.dart
+++ lib/pub_mirror.dart
@@ -1,9 +1,36 @@
+import "dart:io";
 import "dart:convert";
 import "dart:mirrors";
 import "dart:async";
 import "package:pub_client/pub_client.dart";
 
-PubClient _client = new PubClient();
+var _pub_client = PubClient();
+var _http_client = HttpClient();
+
+class StatusCodeException implements Exception {
+  final String url;
+  final int statusCode;
+  final String reason;
+
+  StatusCodeException({
+    this.url = 'some page',
+    this.statusCode = null,
+    this.reason = null,
+  });
+
+  @override
+  String toString() {
+    var buffer = StringBuffer('failed');
+    if (statusCode != null) {
+      buffer.write(' with ${statusCode}');
+    }
+    buffer.write(' when visiting ${url}');
+    if (reason != null) {
+      buffer.write(': ${reason}');
+    }
+    return buffer.toString();
+  }
+}
 
 dynamic SerializeToJson(dynamic object) {
   var toJsonMethod = reflect(object).type.instanceMembers[Symbol("toJson")];
@@ -25,9 +52,24 @@ dynamic SerializeToJson(dynamic object) {
   return object;
 }
 
+Future saveFileTo(String url, String path) async {
+  var request = await _http_client.getUrl(Uri.parse(url));
+  var response = await request.close();
+  if (response.statusCode >= 400) {
+    await response.drain();
+    throw StatusCodeException(
+      reason: response.reasonPhrase,
+      url: url,
+      statusCode: response.statusCode,
+    );
+  }
+  // TODO: progress bar
+  response.pipe(File(path).openWrite());
+}
+
 Stream<Package> listAllPackages() async* {
   for (var i = 1;; i++) {
-    Page package_page = await _client.getPageOfPackages(i);
+    Page package_page = await _pub_client.getPageOfPackages(i);
     for (var package in package_page.packages) {
       yield package;
     }
@@ -38,32 +80,33 @@ Stream<Package> listAllPackages() async* {
 }
 
 Future downloadPackage(String name) async {
-  var full_package = await _client.getPackage(name);
+  var full_package = await _pub_client.getPackage(name);
   // TODO: call
   await savePackageInfo(full_package, '');
   for (var version in full_package.versions) {
     print(
         '--> Downloading ${name}@${version.version} from ${version.archive_url}');
     await saveVersionInfo(version, '');
-    await saveArchiveFile(version.archive_url, '');
+    await saveArchiveFile(version.archive_url, '/dev/null');
   }
 }
 
 Future savePackageInfo(FullPackage pkg, String path) async {
   // TODO: meet the requirements at
   // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L30-L49
-  print(json.encode(SerializeToJson(pkg)));
+  //print('${pkg.url} ==> ${json.encode(SerializeToJson(pkg))}');
 }
 
 Future saveVersionInfo(Version ver, String path) async {
   // TODO: meet the requirements at
   // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L53-L65
-  print(json.encode(SerializeToJson(ver)));
+  //print('${ver.url} ==> ${json.encode(SerializeToJson(ver))}');
 }
 
 Future saveArchiveFile(String url, String path) async {
   // TODO: meet the requirements at
   // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L69-L75
+  await saveFileTo(url, path);
 }
 
 Future downloadAll() async {
