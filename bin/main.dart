268e5a85dc104afe6b3d6ba23d4a9facfe152235
Hui Yiqun <i@huiyiqun.me>
2019-02-23 16:18:46 +0800

    handle options from the cmd line interface

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,30 @@
+import 'dart:io';
 import 'package:pub_mirror/pub_mirror.dart' as pub_mirror;
+import 'package:args/args.dart';
+
+ArgResults parseArgs(List<String> arguments) {
+  var parser = ArgParser()
+    ..addFlag('help', abbr: 'h', negatable: false, help: 'print usage and exit')
+    ..addOption('upstream',
+        abbr: 'u',
+        help: 'set the upstream to mirror from',
+        defaultsTo: 'https://pub.dartlang.org/api',
+        valueHelp: 'UPSTREAM');
+  var result = parser.parse(arguments);
+  if (result['help'] || result.rest.length != 2) {
+    print("""Usage:
+pub_mirror [options] <dest-path> <serving-url>
+
+Example: pub_mirror /tmp/pub/ file:///tmp/pub/
+
+Options:
+${parser.usage}""");
+    exit(0);
+  }
+  return result;
+}
 
 main(List<String> arguments) async {
-  await pub_mirror.downloadAll();
+  var args = parseArgs(arguments);
+  await pub_mirror.PubMirrorTool(args['upstream'], args.rest[0], args.rest[1]).download();
 }
--- lib/pub_mirror.dart
+++ lib/pub_mirror.dart
@@ -2,10 +2,8 @@ import "dart:io";
 import "dart:convert";
 import "dart:mirrors";
 import "dart:async";
+import "package:http/http.dart";
 import "package:pub_client/pub_client.dart";
-
-var _pub_client = PubClient();
-var _http_client = HttpClient();
 
 class StatusCodeException implements Exception {
   final String url;
@@ -32,86 +30,97 @@ class StatusCodeException implements Exception {
   }
 }
 
-dynamic SerializeToJson(dynamic object) {
-  var toJsonMethod = reflect(object).type.instanceMembers[Symbol("toJson")];
-  if (toJsonMethod != null && toJsonMethod.isRegularMethod) {
-    object = object.toJson();
+class PubMirrorTool {
+  final String upstream, destination, serving_url;
+  final _http_client = HttpClient();
+  PubClient _pub_client;
+
+  PubMirrorTool(this.upstream, this.destination, this.serving_url) {
+    _pub_client = PubClient(
+        client: IOClient(_http_client), baseApiUrl: this.upstream);
   }
 
-  if (object is List) {
-    object = object.map(SerializeToJson).toList();
-  }
-
-  if (object is Map) {
-    object = Map<String, dynamic>.fromIterable(
-        object.entries.where((entry) => entry.value != null),
-        key: (entry) => SerializeToJson(entry.key),
-        value: (entry) => SerializeToJson(entry.value));
-  }
-
-  return object;
-}
-
-Future saveFileTo(String url, String path) async {
-  var request = await _http_client.getUrl(Uri.parse(url));
-  var response = await request.close();
-  if (response.statusCode >= 400) {
-    await response.drain();
-    throw StatusCodeException(
-      reason: response.reasonPhrase,
-      url: url,
-      statusCode: response.statusCode,
-    );
-  }
-  // TODO: progress bar
-  response.pipe(File(path).openWrite());
-}
-
-Stream<Package> listAllPackages() async* {
-  for (var i = 1;; i++) {
-    Page package_page = await _pub_client.getPageOfPackages(i);
-    for (var package in package_page.packages) {
-      yield package;
+  dynamic SerializeToJson(dynamic object) {
+    var toJsonMethod = reflect(object).type.instanceMembers[Symbol("toJson")];
+    if (toJsonMethod != null && toJsonMethod.isRegularMethod) {
+      object = object.toJson();
     }
-    if (package_page.next_url == null || package_page.next_url.trim() == "") {
-      break;
+
+    if (object is List) {
+      object = object.map(SerializeToJson).toList();
+    }
+
+    if (object is Map) {
+      object = Map<String, dynamic>.fromIterable(
+          object.entries.where((entry) => entry.value != null),
+          key: (entry) => SerializeToJson(entry.key),
+          value: (entry) => SerializeToJson(entry.value));
+    }
+
+    return object;
+  }
+
+  Future saveFileTo(String url, String path) async {
+    var request = await _http_client.getUrl(Uri.parse(url));
+    var response = await request.close();
+    if (response.statusCode >= 400) {
+      await response.drain();
+      throw StatusCodeException(
+        reason: response.reasonPhrase,
+        url: url,
+        statusCode: response.statusCode,
+      );
+    }
+    // TODO: progress bar
+    response.pipe(File(path).openWrite());
+  }
+
+  Stream<Package> listAllPackages() async* {
+    for (var i = 1;; i++) {
+      Page package_page = await _pub_client.getPageOfPackages(i);
+      for (var package in package_page.packages) {
+        yield package;
+      }
+      if (package_page.next_url == null || package_page.next_url.trim() == "") {
+        break;
+      }
     }
   }
-}
 
-Future downloadPackage(String name) async {
-  var full_package = await _pub_client.getPackage(name);
-  // TODO: call
-  await savePackageInfo(full_package, '');
-  for (var version in full_package.versions) {
-    print(
-        '--> Downloading ${name}@${version.version} from ${version.archive_url}');
-    await saveVersionInfo(version, '');
-    await saveArchiveFile(version.archive_url, '/dev/null');
+  Future downloadPackage(String name) async {
+    var full_package = await _pub_client.getPackage(name);
+    // TODO: call
+    await savePackageInfo(full_package, '');
+    for (var version in full_package.versions) {
+      print(
+          '--> Downloading ${name}@${version.version} from ${version.archive_url}');
+      await saveVersionInfo(version, '');
+      await saveArchiveFile(version.archive_url, '/dev/null');
+    }
   }
-}
 
-Future savePackageInfo(FullPackage pkg, String path) async {
-  // TODO: meet the requirements at
-  // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L30-L49
-  //print('${pkg.url} ==> ${json.encode(SerializeToJson(pkg))}');
-}
+  Future savePackageInfo(FullPackage pkg, String path) async {
+    // TODO: meet the requirements at
+    // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L30-L49
+    //print('${pkg.url} ==> ${json.encode(SerializeToJson(pkg))}');
+  }
 
-Future saveVersionInfo(Version ver, String path) async {
-  // TODO: meet the requirements at
-  // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L53-L65
-  //print('${ver.url} ==> ${json.encode(SerializeToJson(ver))}');
-}
+  Future saveVersionInfo(Version ver, String path) async {
+    // TODO: meet the requirements at
+    // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L53-L65
+    //print('${ver.url} ==> ${json.encode(SerializeToJson(ver))}');
+  }
 
-Future saveArchiveFile(String url, String path) async {
-  // TODO: meet the requirements at
-  // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L69-L75
-  await saveFileTo(url, path);
-}
+  Future saveArchiveFile(String url, String path) async {
+    // TODO: meet the requirements at
+    // https://github.com/dart-lang/pub_server/blob/master/lib/shelf_pubserver.dart#L69-L75
+    await saveFileTo(url, path);
+  }
 
-Future downloadAll() async {
-  await for (var package in listAllPackages()) {
-    print('Downloading ${package.name}');
-    await downloadPackage(package.name);
+  Future download() async {
+    await for (var package in listAllPackages()) {
+      print('Downloading ${package.name}');
+      await downloadPackage(package.name);
+    }
   }
 }
