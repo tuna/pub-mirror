d1cfbeb55edff7ce83b707dc451923e9137048e1
Hui Yiqun <i@huiyiqun.me>
2019-03-28 14:12:32 +0800

    progress bar for downloading

--- lib/http.dart
+++ lib/http.dart
@@ -1,4 +1,7 @@
 import 'dart:io' as io;
+import 'dart:math' as math;
+
+import './logging.dart';
 
 class StatusCodeException implements Exception {
   final String url;
@@ -25,8 +28,19 @@ class StatusCodeException implements Exception {
   }
 }
 
+String getSize(num byte) {
+  final units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  for (int i = 0; i < units.length; i++) {
+    final base = math.pow(1024, i);
+    if (byte < base * 1024 || i + 1 == units.length) {
+      return '${(byte / base).toStringAsFixed(2)} ${units[i]}';
+    }
+  }
+  return 'NaN';
+}
+
 /// download file progressively
-Future saveFileTo(String url, String path, {io.HttpClient client}) async {
+Future saveFileTo(String url, String destination, {io.HttpClient client}) async {
   if (client == null) {
     client = io.HttpClient();
   }
@@ -40,6 +54,21 @@ Future saveFileTo(String url, String path, {io.HttpClient client}) async {
       statusCode: response.statusCode,
     );
   }
-  // TODO: progress bar
-  await response.pipe(io.File(path).openWrite());
+
+  final io_sink = io.File(destination).openWrite();
+  final watch = Stopwatch()..start();
+  var downloaded_length = 0;
+  final progress_printer = Stream.periodic(Duration(seconds: 1)).listen((_) {
+    final seconds = watch.elapsed.inSeconds;
+    logger.info('[${url} ===> ${destination}] ${(100 * downloaded_length / response.contentLength).toStringAsFixed(2)}% Total: ${getSize(response.contentLength)} Downloaded: ${getSize(downloaded_length)} Speed: ${getSize(downloaded_length/seconds)}/s');
+  });
+  try {
+    await for (var block in response) {
+      io_sink.add(block);
+      downloaded_length += block.length;
+    }
+  } finally {
+    await io_sink.close();
+    await progress_printer.cancel();
+  }
 }
