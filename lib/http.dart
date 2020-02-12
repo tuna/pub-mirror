a3048c0f727f6c119c2b98ba400d82a2d5466c1e
Hui Yiqun <i@huiyiqun.me>
2020-02-12 14:47:22 +0800

    with retry and quiver

--- lib/http.dart
+++ lib/http.dart
@@ -1,6 +1,8 @@
 import 'dart:io' as io;
 import 'dart:math' as math;
 import 'dart:async' as async;
+import 'package:quiver/async.dart' as quiver_async;
+import 'package:retry/retry.dart' as retry;
 
 import './logging.dart';
 
@@ -45,54 +47,48 @@ Future saveFileTo(String url, String destination, {io.HttpClient client}) async 
   if (client == null) {
     client = io.HttpClient();
   }
-  io.HttpClientResponse response;
-  for (var retry = 3; retry >= 0; retry--) {
-    try {
-      logger.fine("Connecting to ${url}");
-      var request = await client.getUrl(Uri.parse(url)).timeout(Duration(seconds: 10));
-      logger.fine("Connection has been established: ${url}");
-      response = await request.close().timeout(Duration(seconds: 10));
-      logger.fine("Response has been received: ${url}");
-      if (response.statusCode >= 400) {
-        logger.fine("Non-2xx status code: ${url}");
-        await response.drain();
-        throw StatusCodeException(
-          reason: response.reasonPhrase,
-          url: url,
-          statusCode: response.statusCode,
-        );
-      }
-      break;
-    } on async.TimeoutException catch (e) {
-      if (retry == 0) {
-        logger.warning("Failed to connect to ${url}");
-        rethrow;
-      }
-      logger.info("Timeout connecting to ${url}, retrying (${retry})...");
-    }
+
+  io.HttpClientResponse response = await retry.RetryOptions(maxAttempts: 3).retry(() async {
+    logger.fine("Connecting to ${url}");
+    final request = await client.getUrl(Uri.parse(url)).timeout(Duration(seconds: 10));
+    logger.fine("Connection has been established: ${url}");
+    return await request.close().timeout(Duration(seconds: 10));
+  }, retryIf: (e) => e is async.TimeoutException);
+  logger.fine("Response has been received: ${url}");
+
+  if (response.statusCode >= 400) {
+    logger.fine("Non-2xx status code: ${url}");
+    await response.drain();
+    throw StatusCodeException(
+      reason: response.reasonPhrase,
+      url: url,
+      statusCode: response.statusCode,
+    );
   }
 
+  // save response to buffer
   final watch = Stopwatch()..start();
-  List<int> buffer = [];
-  var downloaded_length = 0;
+  final buffer = quiver_async.StreamBuffer<int>();
   final progress_printer = Stream.periodic(Duration(seconds: 1)).listen((_) {
     final seconds = watch.elapsed.inSeconds;
+    final downloaded_length = buffer.buffered;
     logger.info('[${url} ===> ${destination}] ${(100 * downloaded_length / response.contentLength).toStringAsFixed(2)}% Total: ${getSize(response.contentLength)} Downloaded: ${getSize(downloaded_length)} Speed: ${getSize(downloaded_length/seconds)}/s');
   });
   try {
-    await for (var block in response.timeout(Duration(seconds: 10))) {
-      buffer.addAll(block);
-      downloaded_length += block.length;
-    }
+    await buffer.addStream(response.timeout(Duration(seconds: 10)));
+    await buffer.close();
   } catch (e) {
     print('Unhandled exception during downloading: $e');
     rethrow;
   } finally {
     await progress_printer.cancel();
   }
-  assert(downloaded_length == response.contentLength);
+  assert(buffer.buffered == response.contentLength);
+
+  // save content in buffer to file
   final io_sink = io.File(destination).openWrite();
-  io_sink.add(buffer);
+  io_sink.add(await buffer.read(buffer.buffered));
   await io_sink.close();
-  logger.info('[${url} ===> ${destination}] ${getSize(downloaded_length)} saved.');
+
+  logger.info('[${url} ===> ${destination}] ${getSize(response.contentLength)}(${getSize(response.contentLength/watch.elapsed.inSeconds)}/s) saved.');
 }
